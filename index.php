880d5b78175f951ddc0b7fd22cc065646d19fade
yt5ytt <yt5ytt@gmail.com>
2018-06-02 23:22:14 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+  include('header.php');
+?>
+    <title>AlexWEBSoft - Home</title>
+  </head>
+  <body>
+    <div id="header">
+      <div id="top-line" class="container-fluid">
+        <div class="container">
+          <div class="row align-items-center">
+            <div id="logo"class="col-lg-6">
+              <i class="fas fa-mobile-alt"></i> &nbsp; 381.60.599.1001 &nbsp; &nbsp; &nbsp;
+              <i class="fas fa-envelope"></i> &nbsp; alexwebsoft.net@gmail.com
+            </div>
+            <div id="nav" class="col-lg-6">
+              <nav class="nav justify-content-end align-middle">
+                <a class="nav-link active" href="#">Home</a>
+                <a class="nav-link" href="#">Portfolio</a>
+                <a class="nav-link" href="#">Contact</a>
+                <a class="nav-link disabled" href="#">v.1.0</a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div><!--kraj top-line diva-->
+      <div class="row">
+        <div class="container">
+          <div id="banner">
+            <h1 id="naslov"><b>AlexWEBSoft</b><small>.net</small></h1>
+            <h3>PHP Fulstack Web Developer</h3>
+            <h3>Responsive Web Designer</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<?php
+  include('footer.php');
+?>
